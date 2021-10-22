4fbf3a4e2ae1ef2096ce6a64b356114a46cb2c6d
segfault2.0 <mathias.tabo1@gmail.com>
2021-10-22 17:43:57 +0200

    add: Linter norme

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -1,43 +1,60 @@
 import {
-  SubscribeMessage, WebSocketGateway, OnGatewayInit, WebSocketServer, OnGatewayConnection,
+  SubscribeMessage, WebSocketGateway, OnGatewayInit, WebSocketServer,
 } from '@nestjs/websockets';
 import { Socket, Server } from 'socket.io';
-import { ConsoleLogger, Logger, Type } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { MongoRepository, ObjectID } from 'typeorm';
-import { ObjectId } from 'mongoose';
-import Piece from './entity/piece.entity';
-import PieceModule from './Piece/piece.module';
+import { MongoRepository } from 'typeorm';
 import Game from './entity/game.entity';
-import GameController from './game/game.controller';
 
 type Pos = {
   x: number,
   y: number,
 };
 
-// type Piece = {
-//   x: number,
-//   y: number,
-//   id: number,
-//   color: boolean
-// };
+type Piece = {
+  x: number,
+  y: number,
+  id: number,
+  color: boolean
+};
 
-@WebSocketGateway({ namespace: '/chat' })
-export class ChessGateway implements OnGatewayInit {
+function gameHasPlayer(players: string[], player: string): boolean {
+  for (let i: number = 0; i < players.length; i += 1) {
+    if (player === players[i]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function isSelectPosGood(allPos: Pos[], x: number, y: number): boolean {
+  for (let i: number = 0; i < allPos.length; i += 1) {
+    if (allPos[i].x === x && allPos[i].y === y) {
+      return true;
+    }
+  }
+  return false;
+}
+
+@WebSocketGateway({ namespace: '/chess' })
+export default class ChessGateway implements OnGatewayInit {
   @WebSocketServer() wss: Server;
 
   @InjectRepository(Game)
   private readonly gameRepository: MongoRepository<Game>;
-
-  @InjectRepository(Piece)
-  private readonly piecesRepository: MongoRepository<Piece>;
 
   private logger: Logger = new Logger('ChatGateway');
 
   private user_by_room: Map<string, number> = new Map<string, number>();
 
   private board_by_room: Map<string, Piece[][]> = new Map<string, Piece[][]>();
+
+  private PosiblePos: Pos[] = undefined;
+
+  private playerConected: Map<string, string[]> = new Map<string, string[]>();
+
+  private targetPos: Pos = undefined;
 
   private games: Map<string, Game> = new Map<string, Game>();
 
@@ -47,130 +64,229 @@ export class ChessGateway implements OnGatewayInit {
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
 
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0],
     [1, 1, 1, 1, 1, 1, 1, 1],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ];
 
-  afterInit(server: any) {
+  afterInit() {
     this.logger.log('Initialized!');
-  }
-
-  findePieceByPos(pos_x: number, pos_y: number, pieces: Piece[]): Piece {
-    for (let i = 0; i < pieces.length; i++) {
-      if (pieces[i].x === pos_x && pieces[i].y === pos_y) {
-        console.log(pieces[i]);
-        return pieces[i];
-      }
-    }
-    return null;
-  }
-
-  gameHasPlayer(players: string[], player: string): boolean {
-    for (let i: number = 0; i < players.length; i++) {
-      if (player === players[i]) {
-        return true;
-      }
-    }
-    return false;
   }
 
   getMoveFromPos(x: number, y: number, room: string): Pos[] {
     const res: Pos[] = [];
+    const board: Piece[][] = this.board_by_room.get(room);
     const piece: Piece = this.board_by_room.get(room)[y][x];
     if (piece) {
-      if (piece.value === 0) {
-        let axe_y: number = -1;
-        var no_move: number = 6;
-        if (piece.color) {
-          axe_y = -1;
-          var no_move: number = 1;
+      if (piece.id === 1) {
+        let axeY: number = 1;
+        // var noMove: number = 6;
+        let noMove: boolean = false;
+        if ((y === 1 && piece.color === false) || (y === 6 && piece.color === true)) {
+          noMove = true;
         }
-        let pos: Pos;
-        pos.x = piece.x;
-        pos.y = piece.y + 1 * axe_y;
-        res.push(pos1);
-        if (piece.y === no_move) {
-          var pos1: Pos;
-          pos1.x = piece.x;
-          pos1.y = piece.y + 2 * axe_y;
-          res.push(pos1);
+        if (piece.color) {
+          axeY = -1;
+        }
+        if (y + 1 * axeY < 0 || y + 1 * axeY > 7) {
+          return res;
+        }
+        if (board[y + 1 * axeY][x] === null) {
+          res.push({
+            x,
+            y: y + 1 * axeY,
+          });
+          if (noMove && board[y + 2 * axeY][x] === null) {
+            res.push({
+              x,
+              y: y + 2 * axeY,
+            });
+          }
+        }
+        if (piece.x + 1 <= 7 && board[y + 1 * axeY][piece.x + 1] !== null
+          && board[y + 1 * axeY][piece.x + 1].color !== piece.color) {
+          res.push({
+            x: piece.x + 1,
+            y: piece.y + 1 * axeY,
+          });
+        }
+        if (piece.x - 1 >= 0 && board[y + 1 * axeY][piece.x - 1] !== null
+          && board[y + 1 * axeY][piece.x - 1].color !== piece.color) {
+          res.push({
+            x: piece.x - 1,
+            y: piece.y + 1 * axeY,
+          });
         }
       }
     }
     return res;
   }
 
-  @SubscribeMessage('chatToServer')
-  handleMessage(client: Socket, message: { sender: string, room: string, x: number, y: number }) {
+  firstUserJoin(client: Socket, room: string, player: string, game: Game): boolean {
+    if (!game || !game.players || !gameHasPlayer(game.players, player)) {
+      client.emit('joinedRoom', 'Game or player not found !!');
+      this.logger.log('game or player not found !!');
+      return false;
+    }
+
+    // this.logger.log(`player: ${player} conected to room: ${room} nb players connected:
+    // ${this.user_by_room.get(room)} and game of the room is ${game}`);
+    let pionTab: Piece[][] = [];
+    pionTab = [];
+    let color: boolean = false;
+    let pions: Piece[];
+    for (let y: number = 0; y < this.chess.length; y += 1) {
+      pions = [];
+      if (y >= 6) {
+        color = true;
+      }
+      for (let x: number = 0; x < this.chess[y].length; x += 1) {
+        if (this.chess[y][x] !== 0) {
+          const piece: Piece = {
+            x,
+            y,
+            id: this.chess[y][x],
+            color,
+          };
+          pions.push(piece);
+        } else {
+          pions.push(null);
+        }
+      }
+      pionTab.push(pions);
+    }
+    this.board_by_room.set(room, pionTab);
+
+    const players: string[] = [];
+    players.push(player);
+    this.playerConected.set(room, players);
+    this.logger.log('Board: ');
+    this.logger.log(this.board_by_room.get(room));
+    client.join(room);
+    this.user_by_room.set(room, 1);
+    client.emit('joinedRoom', `Joined room ${room}`);
+    return true;
+  }
+
+  secondUserJoin(client: Socket, room: string, player: string, game: Game): boolean {
+    if (this.user_by_room.get(room) !== 1) {
+      client.emit('joinedRoom', 'Cannot be more player than 2 player in a room !!');
+      this.logger.log('game or player not found !!');
+      return false;
+    }
+    if (!game || !game.players || game.players.length !== 2) {
+      client.emit('joinedRoom', 'You must join the game');
+      this.logger.log('game or player not found !!');
+      return false;
+    }
+    if (!gameHasPlayer(game.players, player)) {
+      client.emit('joinedRoom', 'Player not found.');
+      this.logger.log('game or player not found !!');
+      return false;
+    }
+    this.logger.log(`player: ${player} conected to room: ${room}nb players connected: ${this.user_by_room.get(room)}`);
+
+    this.games.set(room, game);
+    client.join(room);
+    this.user_by_room.set(room, this.user_by_room.get(room) + 1);
+    client.emit('joinedRoom', `Joined room ${room}`);
+    return true;
+  }
+
+  @SubscribeMessage('GameToServer')
+  handleMessage(client: Socket, message: { room: string, player: string, x: number, y: number }) {
+    const { room } = message;
+    const { player } = message;
+    const { x } = message;
+    const { y } = message;
+    this.logger.log(`room = ${room}`);
+    this.logger.log(`player = ${player}`);
+    this.logger.log(`room = ${room}`);
+    this.logger.log(`player = ${player}`);
+    this.logger.log(`nb_user = ${this.user_by_room.get(message.room)}`);
+    // this.logger.log(`game_state = ${this.games.get(room).state}`);
+    if (!room || !player) {
+      client.emit('GameToClient', 'room or player must be defined.');
+      this.logger.log('game or player not found !!');
+      return;
+    }
+
+    if (this.user_by_room.get(message.room) === 1) {
+      client.emit('GameToClient', 'Wait second player');
+    }
     if (this.user_by_room.get(message.room) === 2) {
-      console.log(this.games.get(message.room));
-      if (message.sender === this.games.get(message.room).players[this.games.get(message.room).turn]) {
-        if (message.x >= 0 && message.x <= 7 && message.y >= 0 && message.y <= 7) {
-          const mess: Pos[] = this.getMoveFromPos(message.x, message.y, message.room);
-          client.emit('chatToClient', mess);
+      if (this.games.get(room).state === 1) {
+        const game: Game = this.games.get(room);
+        game.state = 2;
+        this.games.set(room, game);
+        this.wss.to(message.room).emit('GameToClient', 'Start');
+        return;
+      }
+      if (this.games.get(room).state === 2) {
+        this.logger.log(this.games.get(room).players[this.games.get(message.room).turn]);
+        this.logger.log(player);
+
+        if (this.games.get(room).players[this.games.get(room).turn] === player) {
+          if (x >= 0 && x <= 7 && y >= 0 && y <= 7) {
+            const board: Piece[][] = this.board_by_room.get(room);
+            if (this.PosiblePos === undefined || this.PosiblePos.length === 0
+               || !isSelectPosGood(this.PosiblePos, x, y)) {
+              if (board[y][x] !== null
+                && ((board[y][x].color === false && this.games.get(room).turn === 1)
+                || (board[y][x].color === true && this.games.get(room).turn === 0))) {
+                this.targetPos = {
+                  x,
+                  y,
+                };
+                this.PosiblePos = this.getMoveFromPos(x, y, room);
+                client.emit('GameToClient', this.PosiblePos);
+              } else {
+                client.emit('GameToClient', []);
+              }
+            } else {
+              board[y][x] = board[this.targetPos.y][this.targetPos.x];
+              board[y][x].x = x;
+              board[y][x].y = y;
+              board[this.targetPos.y][this.targetPos.x] = null;
+              this.PosiblePos = undefined;
+              this.targetPos = undefined;
+              const game: Game = this.games.get(room);
+              game.turn += 1;
+              if (game.turn > 1) {
+                game.turn = 0;
+              }
+              this.wss.to(message.room).emit('GameToClient', board);
+            }
+          }
+        } else {
+          client.emit('GameToClient', 'Not your turn.');
         }
       }
     }
-    //    this.wss.to(message.room).emit('chatToClient', message);
   }
 
   @SubscribeMessage('joinRoom')
   async handleRoomJoin(client: Socket, message: { room: string, player: string }) {
-    // console.log(this.user_by_room.get(room));
+    if (!message || !message.room || !message.player) {
+      client.emit('joinedRoom', 'messsage must be defined corecly');
+      return;
+    }
     const { room } = message;
     const { player } = message;
-    console.log(`room = ${room}`);
-    console.log(`player = ${player}`);
+    this.logger.log(`room = ${room}`);
+    this.logger.log(`player = ${player}`);
 
-    if (this.user_by_room.has(room)) {
-      if (this.user_by_room.get(room) === 1 && this.games.get(room) !== undefined && this.games.get(room).players && this.games.get(room).players.length === 2) {
-        console.log(`connect room = ${room}`);
-        console.log(`first client = ${player}`);
+    const game: Game = await this.gameRepository.findOne({ id_string: room });
 
-        client.join(room);
-        this.user_by_room.set(room, this.user_by_room.get(room) + 1);
-        console.log(`player: ${player} conected to room: ${room}nb players connected: ${this.user_by_room.get(room)}`);
-
-        client.emit('joinedRoom', room);
-      }
-    } else if (this.user_by_room.has(room) === false) {
-      const game: Game = await this.gameRepository.findOne({ id_string: room });
-      console.log(player);
-      console.log(game);
-      if (game !== undefined && game.players) {
-        console.log(`connect room = ${room}`);
-        console.log(`first client = ${player}`);
-        console.log(`game players = ${game.players}`);
-        if (this.gameHasPlayer(game.players, player)) {
-          console.log('players found !!');
-
-          client.join(room);
-          this.user_by_room.set(room, 1);
-          client.emit('joinedRoom', room);
-
-          console.log(`player: ${player} conected to room: ${room}nb players connected: ${this.user_by_room.get(room)}`);
-
-          const pieceInDb: Piece[] = await this.piecesRepository.find({ room });
-          let pions: Piece[][];
-          pions = [];
-          for (let y: number = 0; y < this.chess.length; y++) {
-            let pion: Piece[];
-            pion = [];
-            for (let x: number = 0; x < this.chess[y].length; x++) {
-              pion.push(this.findePieceByPos(x, y, pieceInDb));
-            }
-            pions.push(pion);
-          }
-          this.board_by_room.set(room, pions);
-
-          console.log('Board: ');
-          console.log(this.board_by_room.get(room));
-          this.games.set(room, game);
-        }
+    if (this.user_by_room.has(room) === false) {
+      this.firstUserJoin(client, room, player, game);
+    } else if (this.user_by_room.has(room)) {
+      if (this.playerConected.has(room) && this.playerConected.get(room).length === 1
+       && this.playerConected.get(room)[0] !== player) {
+        this.secondUserJoin(client, room, player, game);
       } else {
-        console.log('players not found !!');
+        client.emit('joinedRoom', 'User has alredy join the room');
       }
     }
   }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,36 +1,29 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AlertController } from './alert.controller';
-import { AlertGateway } from './alert.gateway';
-import { ChessGateway } from './app.gateway';
+import ChessGateway from './app.gateway';
 import Game from './entity/game.entity';
-import Piece from './entity/piece.entity';
 import User from './entity/user.entity';
-import GameController from './game/game.controller';
 import GameModule from './game/game.module';
 import LoginModule from './login/login.module';
-import PieceModule from './Piece/piece.module';
 import RegisterModule from './register/register.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(), 
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mongodb',
-      url: process.env.DB_URL + ':' + process.env.DB_PORT + '/' + process.env.DB_NAME,
+      url: `${process.env.DB_URL}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
       database: process.env.DB_NAME,
       entities: [`${__dirname}/**/*.entity{.ts,.js}`],
       useUnifiedTopology: true,
       useNewUrlParser: true,
     }),
-    TypeOrmModule.forFeature([User, Piece, Game]),
+    TypeOrmModule.forFeature([User, Game]),
     RegisterModule,
     LoginModule,
-    PieceModule,
     GameModule,
   ],
-  controllers: [AlertController],
-  providers: [AlertGateway, ChessGateway],
+  providers: [ChessGateway],
 })
 export default class AppModule {}
